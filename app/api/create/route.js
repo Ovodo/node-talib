ec6f0418acfec8e4230298142ecd97ad47b0af28
ovodo <ovdizzle@gmail.com>
2024-01-13 01:57:58 +0100

    this should work

--- app/api/create/route.js
+++ app/api/create/route.js
@@ -1,24 +1,24 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { storeSymbolsInFile } from "../../../functions/printSymbols";
-import { identifyCandleWicks } from "../../../strategy/wick";
-async function create(request) {
-  // const body = await request.json();
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import { storeSymbolsInFile } from "../../../functions/printSymbols";
+// import { identifyCandleWicks } from "../../../strategy/wick";
+// async function create(request) {
+//   // const body = await request.json();
 
-  try {
-    // storeSymbolsInFile(["ksks"], "ksls.js");
-    const res = await identifyCandleWicks();
-    return Response.json(
-      { data: res, message: "Contact submitted successfully" },
-      { status: 200 }
-    );
-  } catch (error) {
-    console.error("Error Submitting Contact:", error);
+//   try {
+//     // storeSymbolsInFile(["ksks"], "ksls.js");
+//     const res = await identifyCandleWicks();
+//     return Response.json(
+//       { data: res, message: "Contact submitted successfully" },
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     console.error("Error Submitting Contact:", error);
 
-    return Response.json(
-      { message: "An error while sumbitting contact📜" },
-      { status: 500 }
-    );
-  }
-}
+//     return Response.json(
+//       { message: "An error while sumbitting contact📜" },
+//       { status: 500 }
+//     );
+//   }
+// }
 
-export { create as GET };
+// export { create as GET };
